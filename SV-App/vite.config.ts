905be008058934902cd36c32b25bb746c3175895
MarkTroSVSD <mark@svavnc.com>
2025-12-25 15:45:16 -0500

    Update Vite config for build and plugins

--- SV-App/vite.config.ts
+++ SV-App/vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './',
+  plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   },
   build: {
-    target: 'esnext',
     outDir: 'dist',
+    sourcemap: false,
     rollupOptions: {
-      input: '/index.html',
-    },
+      input: './index.html'
+    }
   },
   server: {
     port: 3000
