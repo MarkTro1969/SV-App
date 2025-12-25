b019ef3983ef29cda2e842570bd8741abebc52ad
MarkTroSVSD <mark@svavnc.com>
2025-12-25 15:38:48 -0500

    Add React plugin and modify build settings
    Added React plugin and updated build configuration.

--- SV-App/vite.config.ts
+++ SV-App/vite.config.ts
@@ -1,14 +1,17 @@
-
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: './',
   define: {
-    // This connects the API_KEY you typed in Vercel to the actual app code
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   },
   build: {
     target: 'esnext',
     outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html',
+    },
   },
   server: {
     port: 3000
